edff72b1c6763d2f897b10e3df0238755b52b3e6
blendthink <info@blendthink.dev>
2021-06-13 12:49:08 +0900

    item クラスを作成

---
+++ src/data/item.ts
@@ -0,0 +1,9 @@
+export class Item {
+    readonly name: string;
+    readonly place: string;
+
+    constructor(name: string, place: string) {
+        this.name = name;
+        this.place = place;
+    }
+}
