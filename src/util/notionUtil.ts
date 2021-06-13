f4d2c4f948566b97122462ff5fcb02176a30c264
blendthink <info@blendthink.dev>
2021-06-13 15:09:24 +0900

    NotionUtil としてクラス化

--- src/util/notionUtil.ts
+++ src/util/notionUtil.ts
@@ -5,30 +5,32 @@ import {
     TitlePropertyValue,
 } from '@notionhq/client/build/src/api-types';
 
-export function getPageName(page: Page): string {
-    const propertyValue: PropertyValue = page.properties['名前'];
-    if (!isTitlePropertyValue(propertyValue)) {
-        throw TypeError;
+export class NotionUtil {
+    static getPageTitle(page: Page, key: string): string {
+        const propertyValue: PropertyValue = page.properties[key];
+        if (!NotionUtil.isTitlePropertyValue(propertyValue)) {
+            throw TypeError;
+        }
+        return propertyValue.title[0].plain_text;
     }
-    return propertyValue.title[0].plain_text;
-}
 
-export function getPagePlace(page: Page): string {
-    const propertyValue: PropertyValue = page.properties['場所'];
-    if (!isSelectPropertyValue(propertyValue)) {
-        throw TypeError;
+    static getPageSelectName(page: Page, key: string): string {
+        const propertyValue: PropertyValue = page.properties[key];
+        if (!NotionUtil.isSelectPropertyValue(propertyValue)) {
+            throw TypeError;
+        }
+        return propertyValue.select.name;
     }
-    return propertyValue.select.name;
-}
 
-function isTitlePropertyValue(value: PropertyValue): value is TitlePropertyValue {
-    return (value as TitlePropertyValue).id !== undefined &&
-        (value as TitlePropertyValue).type !== undefined &&
-        (value as TitlePropertyValue).title !== undefined;
-}
+    private static isTitlePropertyValue(value: PropertyValue): value is TitlePropertyValue {
+        return (value as TitlePropertyValue).id !== undefined &&
+            (value as TitlePropertyValue).type !== undefined &&
+            (value as TitlePropertyValue).title !== undefined;
+    }
 
-function isSelectPropertyValue(value: PropertyValue): value is SelectPropertyValue {
-    return (value as SelectPropertyValue).id !== undefined &&
-        (value as SelectPropertyValue).type !== undefined &&
-        (value as SelectPropertyValue).select !== undefined;
+    private static isSelectPropertyValue(value: PropertyValue): value is SelectPropertyValue {
+        return (value as SelectPropertyValue).id !== undefined &&
+            (value as SelectPropertyValue).type !== undefined &&
+            (value as SelectPropertyValue).select !== undefined;
+    }
 }
