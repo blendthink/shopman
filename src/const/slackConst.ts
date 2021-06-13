9912b6521020bb1d31b497defacfee65095822a8
blendthink <info@blendthink.dev>
2021-06-13 12:48:57 +0900

    slack const を追加

---
+++ src/const/slackConst.ts
@@ -0,0 +1,6 @@
+export const CALLBACK_ID_ADD_ITEM = 'callback_add_item';
+
+export const BLOCK_ID_NAME = 'block_name';
+export const ACTION_ID_NAME = 'action_name';
+export const BLOCK_ID_PLACE = 'block_place';
+export const ACTION_ID_PLACE = 'action_place';
